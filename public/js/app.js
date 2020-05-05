25a3040de676b6cc2a87d7fff20cc9fc4c06c34f
Welton Santos <weltonwms@gmail.com>
2020-05-05 10:51:44 -0300

    add functionalidade busca CEP

--- public/js/app.js
+++ public/js/app.js
@@ -36,5 +36,33 @@ $(document).ready(function () {
     $('.phone').mask(SPMaskBehavior, spOptions);
     $('.money').mask('000.000.000.000.000,00', {reverse: true});
 
+    //função busca CEP em Webservice
+    $("#cep").keyup(function () {
+        if (this.value.length == 9) {
+            $('#cep').css({'background': "url('../img/preload.GIF') no-repeat right", 'background-size': '30px 30px'});
+            $('body').append("<div class='fundo_preload  modal-backdrop fade show'></div>");
+            $.getJSON("//viacep.com.br/ws/" + this.value + "/json/?callback=?", function (dados) {
+
+                if (!("erro" in dados)) {
+                    var string_endereco= dados.logradouro+' '+dados.complemento+' ';
+                    string_endereco+=dados.bairro+' '+dados.localidade+' - '+dados.uf+' '+dados.unidade;
+                    $("#endereco").val(string_endereco);
+                } else {
+
+                    alert("CEP não encontrado.");
+                }
+            }).done(function () {
+                $('#cep').css('background', "url()");
+                $('.fundo_preload').remove();
+            }).fail(function () {
+               $('#cep').css('background', "url()");
+               $('.fundo_preload').remove();
+                console.log('falha de rede ou erro lançado pelo webservice');
+                
+            });
+        }
+    }); //fim  KEYUP função busca CEP
+    //FIM BUSCA CEP
+
 });
 
