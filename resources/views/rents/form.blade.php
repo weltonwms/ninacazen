af27f7a6db12b658f50eeebcbaa096b5222990ed
Welton Santos <weltonwms@gmail.com>
2020-05-27 18:23:14 -0300

    calculo total em produtos para alugar

--- resources/views/rents/form.blade.php
+++ resources/views/rents/form.blade.php
@@ -15,7 +15,7 @@
 </div>
 {{ Form::hidden('produtos_json',null,['class'=>"form-control", 'id'=>'produtos_json']) }}
 @error('produtos_json')
-    <div class="alert alert-danger">{{ $message }}</div>
+<div class="alert alert-danger">{{ $message }}</div>
 @enderror
 
 
@@ -32,12 +32,12 @@
 </div>
 
 <div class="table-responsive">
-    <table class="table table-bordered table-condensed">
-        <thead>
+    <table class="table table-bordered table-sm">
+        <thead class="thead-light">
             <tr>
                 <th>#</th>
                 <th>Produto</th>
-                 <th>Qtd</th>
+                <th>Qtd</th>
                 <th>Valor Un</th>
                 <th>Total</th>
                 <th><i class="fa fa-edit"></i></th>
@@ -49,6 +49,15 @@
         <tbody id="tbodyTableProduto">
 
         </tbody>
+        
+        <tfoot>
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-center table-primary"><b>Total Geral</b></td>
+                <td class="table-primary"><span id="total_geral_tabela">0</span></td>
+                <td colspan="2"></td>
+            </tr>
+        </tfoot>
     </table>
 </div>
 
@@ -82,11 +91,11 @@
                         </div>
                         <div class="form-group col-md-4">
                             <label for="formProduto_qtd" class="col-form-label">Qtd:</label>
-                            <input type="text" class="form-control" id="formProduto_qtd">
+                            <input type="number" class="form-control" id="formProduto_qtd">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="formProduto_valor" class="col-form-label">Valor Un. Aluguel:</label>
-                            <input type="text" class="form-control" id="formProduto_valor_aluguel">
+                            <input type="text" class="form-control money" id="formProduto_valor_aluguel">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="formProduto_total" class="col-form-label">Total:</label>
@@ -128,22 +137,41 @@
         /*
          * Metodos de uma possivel outra classe. mexe com DOM
          */
-        function inicializeItems(){
-            var itemsJson= $("#produtos_json").val() || '[]';
-            var itemsObj= JSON.parse(itemsJson);
-            items=itemsObj;
+        function inicializeItems() {
+            var itemsJson = $("#produtos_json").val() || '[]';
+            var itemsObj = JSON.parse(itemsJson);
+            items = itemsObj;
             updateTableProduto();
-        }        
+        }
         
+        function getTotalGeral(){
+            var map= items.map(function(item, i){
+                return parseFloat(getTotalItem(i));
+            });
+            var soma= map.reduce(function(a,b){
+               return a + b;
+            },0)
+            return soma;
+        }
         
+        function getTotalItem(index) {
+            var item = items[index];
+            if (item) {
+                var total = parseInt(item.qtd) * parseFloat(item.valor_aluguel);
+                return total.toFixed(2);
+            }
+        }
+       
+
         function saveItem() {
             var id = $("#formProduto_id").val().trim();
             var produto_id = $("#formProduto_produto_id").val();
             var qtd = $("#formProduto_qtd").val();
-            var valor_aluguel = $("#formProduto_valor_aluguel").val();
+            var valor_aluguel = ler_valor("#formProduto_valor_aluguel");
             var item = {produto_id: produto_id, qtd: qtd, valor_aluguel: valor_aluguel};
             if (id) {
                 updateItem(item, id);
+                $('#ModalFormProduto').modal('hide');
             } else {
                 addItem(item);
             }
@@ -156,10 +184,10 @@
             var item = items[index];
             $("#formProduto_produto_id").val(item.produto_id);
             $("#formProduto_qtd").val(item.qtd);
-            $("#formProduto_valor_aluguel").val(item.valor_aluguel);
+            $("#formProduto_valor_aluguel").val(valorFormatado(item.valor_aluguel));
             $("#formProduto_id").val(index);
-            console.log('item', item);
-            $('#ModalFormProduto').modal('show')
+
+            $('#ModalFormProduto').modal('show');
 
         }
 
@@ -173,18 +201,19 @@
             var itemsString = JSON.stringify(items);
             $("#produtos_json").val(itemsString);
             var tableItems = items.map(function (item, i) {
-                var product=getObjProduct(item.produto_id);
+                var product = getObjProduct(item.produto_id);
                 return "<tr>" +
                         "<td>" + (i + 1) + "</td>" +
                         "<td>" + product.nome + "</td>" +
                         "<td>" + item.qtd + "</td>" +
-                        "<td>" + item.valor_aluguel + "</td>" +
-                        "<td>" + item.qtd + "</td>" +
+                        "<td>" + valorFormatado(item.valor_aluguel) + "</td>" +
+                        "<td>" + valorFormatado(getTotalItem(i)) + "</td>" +
                         '<td><a href="#" editProduct="' + i + '"> <i class="fa fa-edit"></i>  </a></td>' +
                         '<td><a href="#" deleteProduct="' + i + '"> <i class="fa fa-trash text-danger"></i>  </a></td>' +
                         "</tr>"
             });
             $("#tbodyTableProduto").html(tableItems);
+            $("#total_geral_tabela").html(valorFormatado(getTotalGeral()));
             $("a[editProduct]").click(editProduct);
             $("a[deleteProduct]").click(deleteProduct);
             //console.log(tableItems);
@@ -195,6 +224,7 @@
             $("#formProduto_produto_id").val('');
             $("#formProduto_qtd").val('');
             $("#formProduto_valor_aluguel").val('');
+            $("#formProduto_total").val('');
         }
 
         return {
@@ -205,7 +235,9 @@
             updateTableProduto: updateTableProduto,
             saveItem: saveItem,
             resetFormProduto: resetFormProduto,
-            inicializeItems:inicializeItems
+            inicializeItems: inicializeItems,
+            getTotalItem: getTotalItem,
+            getTotalGeral:getTotalGeral
         };
 
     }
@@ -218,6 +250,34 @@
             return produto;
         }
     }
+
+    function calculoTotal() {
+        var qtd = ler_valor("#formProduto_qtd");
+        var valor_aluguel = ler_valor("#formProduto_valor_aluguel");
+        console.log('qtd', qtd);
+        console.log('valor_aluguel', valor_aluguel); 
+        if (qtd && valor_aluguel) {
+            var total = qtd * valor_aluguel;
+            
+            $("#formProduto_total").val(valorFormatado(total));
+
+        } else {
+            $("#formProduto_total").val('');
+        }
+    }
+    
+    function valorFormatado(valorNumber){
+        var v= Number.parseFloat(valorNumber); //garantindo que param vai ser number
+        var valor_formatado = v.toFixed(2).toString().replace('.', ',');
+        return valor_formatado; //string formatada
+    }
+    
+    function ler_valor(campo) {
+        var valor = $(campo).val().replace('.', '').replace(',', '.');
+        return parseFloat(valor);
+    }
+    
+    
 
     var obj = Produtos();
     obj.inicializeItems();
@@ -233,8 +293,13 @@
     });
 
     $('#formProduto_produto_id').change(function (e) {
-        var produto= getObjProduct(this.value);
-        $("#formProduto_valor_aluguel").val(produto.valor_aluguel);
+        var produto = getObjProduct(this.value);
+        $("#formProduto_valor_aluguel").val(valorFormatado(produto.valor_aluguel));
+        calculoTotal();
+    });
+
+    $("#formProduto_qtd, #formProduto_valor_aluguel ").on("change", function () {
+        calculoTotal();
     });
 
 </script>
